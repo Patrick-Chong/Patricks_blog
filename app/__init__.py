a3d3967235a8f578bd12a1c505f5781148c09212
PatrickChong <patrick_chong@hotmail.co.uk>
2021-09-15 12:29:19 +0100

    heroku deployment changes

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from config import Config
 from flask_moment import Moment
+from app import routes, models, errors
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -54,18 +55,4 @@ if not app.debug:
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
 
-        
 
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
-                                       backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Microblog startup')
-
-from app import routes, models, errors
